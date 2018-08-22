19bf1af8cb88a7f3f883356c70eef68f81fe7448
Sam Ashtar <samashtarm@gmail.com>
2018-08-22 21:15:59 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,8 +19,7 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
         data.delete_if do |flavor|
          flavor == "strawberry"
-            
-    
+
       end
     end
   end
