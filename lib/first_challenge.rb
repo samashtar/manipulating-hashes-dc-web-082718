b866882217a21d9ddb1555ab647ad0449a0ece46
Sam Ashtar <samashtarm@gmail.com>
2018-08-22 20:48:57 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,15 @@ def first_challenge
     }
   }
 
-  data.each do |attribute, value|
-    if :favorite_icecream_flavors = "strawberry"
-      
-
+def remove_strawberry(contacts)
+  contacts.each do |name, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_ice_cream_flavors
+        data[:favorite_ice_cream_flavors].delete_if { |i| i == "strawberry" }
+      end
+    end
+  end
+end
 
   #remember to return your newly altered contacts hash!
   contacts
