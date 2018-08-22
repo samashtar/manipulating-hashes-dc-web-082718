7367f71d9295231a2d0ecba469def6e8e22c8247
Sam Ashtar <samashtarm@gmail.com>
2018-08-22 21:13:23 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
     }
   }
 
-  contacts.each do |name, data|
+  contacts["Freddy Mercury"].each do |name, data|
     data.each do |attribute, value|
       if attribute == :favorite_ice_cream_flavors
         value.delete if value == "strawberry"
